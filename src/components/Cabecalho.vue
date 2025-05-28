24ff56f8490b2efdfdfb28f101d7d3da8ad98f06
Pedro Costa <pedrohefc@gmail.com>
2025-05-28 19:04:32 -0300

    componentizando a aplicação

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    estado: Object
+})
+</script>
+
+<template>
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+        <h1>Minhas tarefas</h1>
+        <p>
+            Você possui {{ estado.tarefas.filter(t => !t.finalizada).length }} tarefas pendentes
+        </p>
+    </header>
+</template>
---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const props = defineProps(['tarefas']);
+</script>
+
+<template>
+    <ul class="list-group mt-4">
+        <li v-for="tarefa in props.tarefas" :key="tarefa.titulo" :class="['list-group-item', {done: tarefa.finalizada}]">
+            <input v-model="tarefa.finalizada" :id="tarefa.titulo" type="checkbox">
+            <label class="ms-3" :for="tarefa.titulo">
+                {{ tarefa.titulo }}
+            </label>
+        </li>
+    </ul>
+</template>
